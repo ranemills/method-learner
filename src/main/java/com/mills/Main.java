76c0f7c94195f3a9cea5b315437aceb73abfce83
Ryan Mills <ranemills@gmail.com>
2017-07-21 12:08:57 +0100

    Add SpringApplication.run

--- src/main/java/com/mills/Main.java
+++ src/main/java/com/mills/Main.java
@@ -1,11 +1,12 @@
 package com.mills;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SpringApplication.run(Main.class);
     }
 }
