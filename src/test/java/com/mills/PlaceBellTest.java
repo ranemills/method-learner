7d91a4cf38e6ef1ff70d8529351a9172e063a2c9
Ryan Mills <ranemills@gmail.com>
2017-07-21 11:35:13 +0100

    Test refactoring

--- src/test/java/com/mills/PlaceBellTest.java
+++ src/test/java/com/mills/PlaceBellTest.java
@@ -4,19 +4,12 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
-import static com.google.common.collect.Sets.powerSet;
 import static com.mills.PlaceBellNumber.FIVE;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class PlaceBellTest {
@@ -49,40 +42,31 @@ class PlaceBellTest {
 //    @Ignore
     void loggingWithTopRating()
     {
-        List<List<DateTime>> scenarios = new ArrayList<>();
 
-        DateTime sevenDaysAgo = DateTime.now().minusDays(7);
-        DateTime sixDaysAgo = DateTime.now().minusDays(6);
-        DateTime fiveDaysAgo = DateTime.now().minusDays(5);
-        DateTime fourDaysAgo = DateTime.now().minusDays(4);
-        DateTime threeDaysAgo = DateTime.now().minusDays(3);
-        DateTime twoDaysAgo = DateTime.now().minusDays(2);
-        DateTime oneDayAgo = DateTime.now().minusDays(1);
-        DateTime today = DateTime.now();
-
-        Set<DateTime> availableDays = ImmutableSet.of(sevenDaysAgo, sixDaysAgo, fiveDaysAgo, fourDaysAgo, threeDaysAgo, twoDaysAgo, oneDayAgo, today);
+        Set<DateTime> availableDays = ImmutableSet.of(DateTime.now().minusDays(7),
+                                                      DateTime.now().minusDays(6),
+                                                      DateTime.now().minusDays(5),
+                                                      DateTime.now().minusDays(4),
+                                                      DateTime.now().minusDays(3),
+                                                      DateTime.now().minusDays(2),
+                                                      DateTime.now().minusDays(1),
+                                                      DateTime.now());
 
 
-        for(Set<DateTime> combination : Sets.powerSet(availableDays))
-        {
+        for (Set<DateTime> combination : Sets.powerSet(availableDays)) {
             PlaceBell placeBell = new PlaceBell("Zanussi", FIVE);
 
 
-            for(DateTime date : combination)
-            {
+            for (DateTime date : combination) {
                 placeBell.addLearningResult(LearningResult.of(date, Rating.FIVE));
             }
 
             System.out.print(placeBell.getPlaceBellRating() + ", ");
 
-            for(DateTime day : availableDays)
-            {
-                if(combination.contains(day))
-                {
-                    System.out.print(new Duration(day, today).getStandardDays()+1 + ", ");
-                }
-                else
-                {
+            for (DateTime day : availableDays) {
+                if (combination.contains(day)) {
+                    System.out.print(new Duration(day, DateTime.now()).getStandardDays() + 1 + ", ");
+                } else {
                     System.out.print(", ");
                 }
             }
