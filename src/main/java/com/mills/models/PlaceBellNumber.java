b76b01dd12f95c3e8fbba7bb7c050f501a0cd3a9
Ryan Mills <ranemills@gmail.com>
2017-07-21 16:59:20 +0100

    Add number conversion for PlaceBellNumber

--- src/main/java/com/mills/models/PlaceBellNumber.java
+++ src/main/java/com/mills/models/PlaceBellNumber.java
@@ -1,16 +1,40 @@
 package com.mills.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum PlaceBellNumber {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    ELEVEN,
-    TWELVE;
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    ELEVEN(11),
+    TWELVE(12);
+
+    private Integer _value;
+
+    private static Map<Integer, PlaceBellNumber> intToPlaceBellMap = new HashMap<>();
+
+    static {
+        for(PlaceBellNumber number : values())
+        {
+            intToPlaceBellMap.put(number._value, number);
+        }
+    }
+
+    PlaceBellNumber(Integer value)
+    {
+        _value = value;
+    }
+
+    public static PlaceBellNumber valueOf(Integer number)
+    {
+        return intToPlaceBellMap.get(number);
+    }
 }
